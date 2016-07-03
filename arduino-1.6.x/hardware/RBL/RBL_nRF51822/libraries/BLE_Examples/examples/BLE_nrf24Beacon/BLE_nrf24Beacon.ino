183321a8be1001fe00ce8a127caf61d80e49402d
5shekel <yair99@gmail.com>
2016-07-03 04:33:03 +0300

    merged beacon and scan parser to one nano on the nano24 pcb

--- arduino-1.6.x/hardware/RBL/RBL_nRF51822/libraries/BLE_Examples/examples/BLE_nrf24Beacon/BLE_nrf24Beacon.ino
+++ arduino-1.6.x/hardware/RBL/RBL_nRF51822/libraries/BLE_Examples/examples/BLE_nrf24Beacon/BLE_nrf24Beacon.ino
@@ -19,12 +19,25 @@
 
 #include "funcs.h" ///all NRF functions 
 
+#include "SoftwareSerial.h"
+SoftwareSerial mySerial(8, 7); //RX,TX
+String inputString = "";         // a string to hold incoming data
+int beacon[3] = {0, 0, 0};
+bool gotBeacon = false;  // whether the string is complete
+bool validId = false;
+
 unsigned long previousMillis = 0;
 const long interval = 500;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("restart serial");
 
-  
+
+  mySerial.begin(9600); //serial with only RX pin
+  inputString.reserve(50);
+  mySerial.println("restart");
+
   pinMode(PIN_CSN, OUTPUT);
   pinMode(PIN_CE, OUTPUT);
   pinMode(11, OUTPUT);
@@ -32,15 +45,12 @@ void setup() {
   digitalWrite(PIN_CSN, HIGH);
   digitalWrite(PIN_CE, LOW);
 
-
-  //Serial.begin(9600); //not neccesery
-  //Serial.println("Start LE advertizing");
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
 
   initNRF();
 
-  
+
 
 }
 
@@ -50,5 +60,70 @@ void loop() {
     previousMillis = currentMillis;
     channel_hop();
   }
+  processScan();
 }
+
+void processScan() {
+  if (gotBeacon) {
+    int rssi = getValue(inputString, '|', 0).toInt();
+    int id = getValue(inputString, '|', 1).toInt();
+
+    switch (id) {
+      case 1:
+        beacon[1] = rssi;
+        validId = true;
+        break;
+      case 2:
+        beacon[2] = rssi;
+        validId = true;
+        break;
+      case 3:
+        beacon[3] = rssi;
+        validId = true;
+        break;
+      default:
+        break;
+    }
+    if (validId) {
+      Serial.print("["); Serial.print(id);
+      Serial.print("]:"); Serial.println(rssi);
+      validId = false;
+    }
+    // clear the string:
+    inputString = "";
+    gotBeacon = false;
+
+  }
+  serialEventListener();
+}
+
+void serialEventListener() {
+  while (mySerial.available()) {
+    char inChar = (char)mySerial.read();
+
+    inputString += inChar;
+    if (inChar == '#') {
+      gotBeacon = true;
+    }
+  }
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {
+    0, -1
+  };
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+
 
